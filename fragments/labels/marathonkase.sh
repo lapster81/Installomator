7276db266a84652a17b44c599220ba06f801c723
lapster81 <117836093+lapster81@users.noreply.github.com>
2024-11-25 13:33:26 +0100

    Update marathonkase.sh

--- fragments/labels/marathonkase.sh
+++ fragments/labels/marathonkase.sh
@@ -1,14 +1,20 @@
 marathonkase)
-      name="Marathon Kalin Setterberg Data AB"
-      type="pkg"
-      versionKey="CFBundleVersion"
-      downloadURL_aarch64="https://update.kase.se/marathon/macos-aarch64/Marathon-$(curl -s https://update.kase.se/marathon/macos-aarch64/versions.txt | tail -n 1)_aarch64.pkg"
-      downloadURL_x64="https://update.kase.se/marathon/macos-x64/Marathon-$(curl -s https://update.kase.se/marathon/macos-x64/versions.txt | tail -n 1)_x64.pkg"
-      if [[ $(arch) == "arm64" ]]; then
-        downloadURL=$downloadURL_aarch64
-      else
-        downloadURL=$downloadURL_x64
-      fi
-      appNewVersion=$(curl -s https://update.kase.se/marathon/macos-aarch64/versions.txt | tail -n 1) # Fetch the latest version
-      expectedTeamID="WUUW5HSQMZ"
-      ;;
+    name="Marathon Kalin Setterberg AB"
+    type="pkg"
+    case $(arch) in
+        "arm64")
+          cpu_arch="aarch64"
+        ;;
+        "i386")
+          cpu_arch="x64"
+        ;;
+    esac
+    latestVersion=$(curl -s https://update.kase.se/marathon/macos-${cpu_arch}/versions.txt | tail -n 1)
+    archiveName="Marathon-${latestVersion}_${cpu_arch}.pkg"
+    downloadURL="https://update.kase.se/marathon/macos-${cpu_arch}/${archiveName}"
+    packageID="se.kase.marathon"
+    versionKey="CFBundleVersion"
+    appNewVersion="${latestVersion}"
+    expectedTeamID="WUUW5HSQMZ"
+    blockingProcesses=( "java" "marathon" )
+    ;;
